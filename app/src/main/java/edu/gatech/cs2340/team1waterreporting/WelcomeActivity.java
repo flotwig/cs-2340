06fa56ec7681409975d006431469986898476f06
Zach Bloomquist <zbloomq@live.com>
2017-02-10 19:08:13 -0500

    [M4] Welcome screen and other functionality related to login

---
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package edu.gatech.cs2340.team1waterreporting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void onClickLoginButton(View v) {
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void onClickRegisterButton(View v) {
+
+    }
+}
