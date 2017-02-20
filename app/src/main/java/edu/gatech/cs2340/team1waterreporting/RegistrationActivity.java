d21e679c69d3ec6bba3103dca03a9d591a3237e1
MatthewJColeman42 <MatthewJColeman42@gmail.com>
2017-02-20 11:59:23 -0500

    M5: Created and designed the registration screen. I need to go back and fix some of the text so that it is not hard coded. Made the RegistrationActivity file, but no code is written yet.

---
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs2340.team1waterreporting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Matthew on 2/20/2017.
+ */
+
+public class RegistrationActivity extends AppCompatActivity {
+}
