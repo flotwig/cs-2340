29d6aab62361c5dbec79bdc23d2336f872d9b500
Zach Bloomquist <zbloomq@live.com>
2017-02-21 23:00:29 -0500

    [M5] Implemented saving in EditProfile

--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/EditProfileFragment.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/EditProfileFragment.java
@@ -1,7 +1,5 @@
 package edu.gatech.cs2340.team1waterreporting;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,11 +17,7 @@ import edu.gatech.cs2340.team1waterreporting.model.UserRole;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A fragment which handles the editing of a user object.
  */
 public class EditProfileFragment extends Fragment {
     private static final String ARG_USER = "user";
@@ -35,6 +29,9 @@ public class EditProfileFragment extends Fragment {
 
     private User mUser;
 
+    /**
+     * Creates an empty EditProfileFragment.
+     */
     public EditProfileFragment() {
 
     }
@@ -71,6 +68,8 @@ public class EditProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+        getActivity().setTitle(getString(R.string.title_fragment_edit_profile));
+
         Button mSaveButton = (Button) v.findViewById(R.id.edit_profile_save_button);
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,14 +99,43 @@ public class EditProfileFragment extends Fragment {
 
     public void onClickSaveButton(View v) {
         // check each field for changes and provide some feedback if it was updated
+        EditText errorControl = null;
+        if (!mPassword.getText().toString().isEmpty()) {
+            try {
+                User.validatePassword(mPassword.getText().toString());
+                mUser.setPassword(mPassword.getText().toString());
+            } catch (UserInputException e) {
+                mPassword.setError(e.getMessage());
+                errorControl = mPassword;
+            }
+        }
+
+        if (!mId.getText().toString().equals(mUser.getId())) {
+            try {
+                User.validateId(mId.getText().toString());
+                mUser.setId(mId.getText().toString());
+            } catch (UserInputException e) {
+                mId.setError(e.getMessage());
+                errorControl = mId;
+            }
+        }
+
         if (!mName.getText().toString().equals(mUser.getName())) {
             try {
                 User.validateName(mName.getText().toString());
-
+                mUser.setName(mName.getText().toString());
             } catch (UserInputException e) {
                 mName.setError(e.getMessage());
+                errorControl = mName;
             }
         }
-        // TODO: finish this section
+
+        if (!mRole.getSelectedItem().equals(mUser.getUserRole())) {
+            mUser.setUserRole((UserRole) mRole.getSelectedItem());
+        }
+
+        if (errorControl != null) {
+            errorControl.requestFocus();
+        }
     }
 }
