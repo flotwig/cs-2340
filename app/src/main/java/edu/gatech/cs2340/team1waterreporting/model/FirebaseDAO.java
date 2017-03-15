d37308a6dffd687de911de34c588b7d7fdb798c5
Zach Bloomquist <zbloomq@live.com>
2017-03-15 16:48:09 -0400

    [M9] Cleanup

--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/FirebaseDAO.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/FirebaseDAO.java
@@ -7,7 +7,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/Model.java
@@ -1,11 +1,5 @@
 package edu.gatech.cs2340.team1waterreporting.model;
 
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-
 /**
  * This model serves as the sole data access object for the controllers.
  */
