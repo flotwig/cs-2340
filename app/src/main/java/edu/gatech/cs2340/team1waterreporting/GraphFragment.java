a3e833c442fe3b938edf365eea99c2801441dd5e
tesorrells <trevor.sorrells45@gmail.com>
2017-03-30 17:40:37 -0400

    Graph logic
    Most of the graph logic looks like it works, crashes when clicked

--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/GraphFragment.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/GraphFragment.java
@@ -14,7 +14,17 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import edu.gatech.cs2340.team1waterreporting.model.InMemoryDAO;
 import edu.gatech.cs2340.team1waterreporting.model.Location;
+import edu.gatech.cs2340.team1waterreporting.model.Model;
+import edu.gatech.cs2340.team1waterreporting.model.UserInputException;
+import edu.gatech.cs2340.team1waterreporting.model.WaterPurityReport;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.ParseException;
+import java.util.ArrayList;
 
 /**
  * Created by Trevor on 3/29/2017.
@@ -24,8 +34,8 @@ public class GraphFragment extends Fragment {
 
     private EditText mLatitude;
     private EditText mLongitude;
-    private EditText mVirusPpm;
-    private EditText mContaminentsPpm;
+    private EditText mVirusContaminentPpm;
+    private EditText mYear;
 
     public static GraphFragment newInstance() {
         return new GraphFragment();
@@ -33,29 +43,79 @@ public class GraphFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /*View view = inflater.inflate(R.layout.fragment_graph_data, container, false);
-        Button mSaveButton = (Button) view.findViewById(R.id.new_water_report_save_button);
-        mLatitude = (EditText) view.findViewById(R.id.new_water_report_latitude);
-        mLongitude = (EditText) view.findViewById(R.id.new_water_report_longitude);
-        mVirusPpm = (EditText) view.findViewById(R.id.new_water_report_virusppm);
-        if(mSaveButton.isPressed()) {*/
-            View view = inflater.inflate(R.layout.fragment_graph, container, false);
-            populateGraphView(view);
-        //}
+        View view = inflater.inflate(R.layout.fragment_graph_data, container, false);
+        Button mSaveButton = (Button) view.findViewById(R.id.graph_enter);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSaveButton(v);
+            }
+        });
+
+        mLatitude = (EditText) view.findViewById(R.id.graph_latitude);
+        mLongitude = (EditText) view.findViewById(R.id.graph_longitude);
+        mVirusContaminentPpm = (EditText) view.findViewById(R.id.graph_virus_or_contaminent);
+        mYear = (EditText) view.findViewById(R.id.graph_year);
+        GraphView graph = (GraphView) view.findViewById(R.id.graph1);
+        graph.setVisibility(view.INVISIBLE);
+        if(mSaveButton.isPressed()) {
+            populateGraphView(view, graph);
+            graph.setVisibility(view.VISIBLE);
+        }
 
         return view;
     }
 
-    private void populateGraphView(View view) {
+    private void populateGraphView(View view, GraphView graph) {
 
-        GraphView graph = (GraphView) view.findViewById(R.id.graph1);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+        DataPoint[] reports = new DataPoint[10];
+        int i = 0;
+        for (WaterPurityReport report : Model.getInstance().getWaterPurityReports()) {
+            if (report.getDate().getYear() == Integer.parseInt(mYear.getText().toString())) {
+                DataPoint r = new DataPoint(report.getDate(), report.getVirusPpm());
+                reports[i] = r;
+                i++;
+            }
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(reports);
         graph.addSeries(series);
+    }
+
+    /**
+     * onClick handler that will save the new water report and
+     * transition to the list view or alert the user to any input
+     * validation errors.
+     * @param v Save button
+     */
+    private void onClickSaveButton(View v) {
+        EditText errorControl = null;
+
+        try {
+            Location.validateLatitude(mLatitude.getText().toString());
+        } catch (UserInputException e) {
+            mLatitude.setError(e.getMessage());
+            errorControl = mLatitude;
+        }
+
+        try {
+            Location.validateLongitude(mLongitude.getText().toString());
+        } catch (UserInputException e) {
+            mLongitude.setError(e.getMessage());
+            errorControl = mLongitude;
+        }
+
+        try {
+            WaterPurityReport.validatePpm(mVirusContaminentPpm.getText().toString());
+        } catch (UserInputException e) {
+            mVirusContaminentPpm.setError(e.getMessage());
+            errorControl = mVirusContaminentPpm;
+        }
+
+        try {
+            WaterPurityReport.validatePpm(mVirusContaminentPpm.getText().toString());
+        } catch (UserInputException e) {
+            mVirusContaminentPpm.setError(e.getMessage());
+            errorControl = mVirusContaminentPpm;
+        }
     }
 }
