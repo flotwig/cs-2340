ea722de6c9d6a2ad839f1bee2cc907d8b81b0cb9
MatthewJColeman42 <MatthewJColeman42@gmail.com>
2017-04-05 11:42:00 -0500

    [M10] Fixed class structure issues as found by the checker

--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/ListWaterPurityReportsFragment.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/ListWaterPurityReportsFragment.java
@@ -18,9 +18,6 @@ import edu.gatech.cs2340.team1waterreporting.model.Model;
  */
 public class ListWaterPurityReportsFragment extends Fragment {
 
-    private ListView mList;
-    private FloatingActionButton mNewButton;
-
     /**
      * Creates an empty ListWaterPurityReportsFragment.
      */
@@ -44,7 +41,7 @@ public class ListWaterPurityReportsFragment extends Fragment {
 
         getActivity().setTitle(getString(R.string.title_fragment_list_water_reports));
 
-        mNewButton = (FloatingActionButton) v.findViewById(R.id.fab);
+        FloatingActionButton mNewButton = (FloatingActionButton) v.findViewById(R.id.fab);
         mNewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +49,7 @@ public class ListWaterPurityReportsFragment extends Fragment {
             }
         });
 
-        mList = (ListView) v.findViewById(R.id.list);
+        ListView mList = (ListView) v.findViewById(R.id.list);
         mList.setAdapter(new WaterQualityReportArrayAdapter(getContext(), (ArrayList) Model.getInstance().getWaterPurityReports()));
 
         return v;
--- app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/team1waterreporting/model/Model.java
@@ -4,6 +4,7 @@ package edu.gatech.cs2340.team1waterreporting.model;
  * This model serves as the sole data access object for the controllers.
  */
 
+@SuppressWarnings("ClassWithOnlyPrivateConstructors")
 public class Model extends FirebaseDAO {
     private static Model instance = new Model();
 
